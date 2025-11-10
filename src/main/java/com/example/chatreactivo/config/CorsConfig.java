0897e0d1984c293ca75f542e00d4726397427428
BrayanZabaleta <brayannava100@gmail.com>
2025-11-10 18:12:48 -0500

    Agregar el CorsConfig

--- src/main/java/com/example/chatreactivo/config/CorsConfig.java
+++ src/main/java/com/example/chatreactivo/config/CorsConfig.java
@@ -1,4 +1,24 @@
 package com.example.chatreactivo.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
 public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("*"); // permite cualquier origen
+        corsConfig.addAllowedMethod("*"); // permite todos los métodos (GET, POST, etc.)
+        corsConfig.addAllowedHeader("*"); // permite todos los headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
 }
